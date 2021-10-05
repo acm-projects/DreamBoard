15e4c9f29ef6a7483254c9cdd0054147218bd289
shakthiper <90812914+shakthiper@users.noreply.github.com>
2021-10-04 19:46:27 -0500

    Sign in box and Navigation Bar components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <ul class="header">
+      <li><a href="colorpalette.asp">Upload Color Palette</a></li>
+      <li><a href="furniture.asp">Upload Furniture</a></li>
+      <li><a href="designs.asp">My Designs</a></li>
+      <li style="float: right"><a href="signIn.asp">Log In/Sign Up</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+.header {
+  position: fixed; /* fixing the position takes it out of html flow - knows
+                   nothing about where to locate itself except by browser
+                   coordinates */
+  left: 0; /* top left corner should start at leftmost spot */
+  top: 0; /* top left corner should start at topmost spot */
+  width: 100vw; /* take up the full browser width */
+  z-index: 200; /* high z index so other content scrolls underneath */
+  height: 50px; /* define height for content */
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: rgb(97, 100, 81);
+}
+
+li {
+  float: left;
+}
+
+a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: rgb(72, 73, 60);
+}
+</style>
---
+++ src/components/SignInBox.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+    <img src="@/assets/rustic-living-room.png" alt="rustic room" style="width:140%;">
+    <div class="signInText">
+      <h1>Welcome back to DreamBoard<br>Please sign in</h1>
+      
+    <form>
+      <div class="form-group">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">      </div>
+    </form>
+    
+    <form>
+      <div class="form-group">
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password">      </div>
+    </form>
+
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SignInBox',
+}
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  text-align: center;
+  color: white;
+}
+
+img {
+  opacity: 0.6;
+}
+.signInText {
+  padding: 1px 20px 20px;
+  background-color: rgba(119, 111, 89, 0.87);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.form-group {
+  padding: 3px 5px;
+}
+</style>
