c4926b4519c92fa819ee058e636cc3a2b69db7f8
paxgole <paxgole@gmail.com>
2021-11-08 17:21:45 -0600

    updated schema.js

--- schema.js
+++ schema.js
@@ -1,9 +1,4 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-mongoose.connect('mongodb://localhost:27017/Dreamboard');
-
-const furnitureSchema = new Schema
+const furnitureSchema = 
 ({
     source: String,
     title:  String,
@@ -11,30 +6,85 @@ const furnitureSchema = new Schema
     thumbnail: String, // url
     price: Number
 });
-const collectionSchema = new Schema
+const collectionSchema = 
 ({
     title:  String,
     pieces: Array // array of furnitureSchema
 });
-const userSchema = new Schema
+const userSchema = 
 ({
     emailID: String,
     password: String,
     collections: Array // array of collectionSchema
 });
 
-const User = mongoose.model('User', userSchema);
-const Collection = mongoose.model('Collection', collectionSchema);
-const Furniture = mongoose.model('Furniture', furnitureSchema);
+// const User = mongoose.model('User', userSchema);
+// const Collection = mongoose.model('Collection', collectionSchema);
+// const Furniture = mongoose.model('Furniture', furnitureSchema);
 
-function save(saveData)
+
+
+const { MongoClient } = require('mongodb');
+require('dotenv').config({path:".env"});
+
+const uri = process.env.MONGO_CONN;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function startDB()
 {
-    const newPiece = new Furniture(saveData);
-    newPiece.save;
-    console.log("SAVED");
+    try
+    {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db('test');
+        console.log("done!");
+        
+        var userData =
+        {
+            emailID: "totallyrealperson@yesmail.com",
+            password: "totallySecurePassword"
+        }
+
+        const col = db.collection("users");
+        const p = await addUser(userData,col);
+        // const p = await getUser(userData.emailID,col);
+        col.find()
+
+        console.log(p);
+
+
+
+    }
+    catch (err)
+    {
+        console.log(err.stack);
+    }
+    finally
+    {
+        await client.close();
+    }
 }
+
+async function addUser(userData,col)
+{
+    userData['collections'] = [null];
+    return col.insertOne(userData);
+}
+
+async function deleteUser(id,col)
+{
+
+}
+
+async function getUser(email,col)
+{
+       
+}
+
+startDB().catch(console.dir);
 
 module.exports = 
 {
-    save
+    startDB
 };
