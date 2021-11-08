f382d5fcac71be0ecfdeaf967dced9697c800f34
Shakthi Perera <shakthiperera@Shakthis-MacBook-Pro.local>
2021-11-08 17:41:00 -0600

    Fixed previous commit, added modal

---
+++ dreamboard-app/src/components/Modal.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">
+          Collections
+        </slot>
+        <button
+          type="button"
+          class="btn-close"
+          @click="close"
+        >
+          x
+        </button>
+      </header>
+
+      <section class="modal-body">
+        <slot name="body">
+            <div class="flex-container">
+          <button>+ New collection</button>
+          <button>Default Collection</button>
+          </div>
+        </slot>
+       </section>
+
+      <footer class="modal-footer">
+        <slot name="footer">
+          
+        </slot>
+        <button
+          type="button"
+          class="btn-green"
+          @click="close"
+        >
+          Close
+        </button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Modal',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+
+<style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal-header,
+  .modal-footer {
+    padding: 15px;
+    display: flex;
+  }
+
+  .modal-header {
+    position: relative;
+    border-bottom: 1px solid #eeeeee;
+    color: #439cb3;
+    justify-content: space-between;
+  }
+
+  .modal-footer {
+    border-top: 1px solid #eeeeee;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+    .flex-container {
+     display: flex;
+     flex-direction: row;
+     justify-content: space-around;
+  }
+
+  .btn-close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    border: none;
+    font-size: 20px;
+    padding: 10px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #439cb3;
+    background: transparent;
+  }
+
+  .btn-green {
+    color: white;
+    background: #439cb3;
+    border: 1px solid #439cb3;
+    border-radius: 2px;
+  }
+</style>
---
+++ dreamboard-app/src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="nav">
+    <ul class="header">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/colorpicker">Color Picker</router-link></li>
+      <li><router-link to="/fileupload">File Upload</router-link></li>
+      <li><router-link to="/resultspage">Results Page</router-link></li>
+      <li style="float: right"><router-link to="/savedpage">Saved Page</router-link></li>
+      <li style="float: right"><router-link to="/login">Login</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
+<style scoped>
+.header {
+  position: fixed; /* fixing the position takes it out of html flow - knows
+                   nothing about where to locate itself except by browser
+                   coordinates */
+  left: 0; /* top left corner should start at leftmost spot */
+  top: 0; /* top left corner should start at topmost spot */
+  width: 100vw; /* take up the full browser width */
+  z-index: 200; /* high z index so other content scrolls underneath */
+  height: 50px; /* define height for content */
+}
+#nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  /*background-color: rgb(97, 100, 81);*/
+  background-color: rgb(100, 139, 161);
+}
+
+#nav li {
+  float: left;
+}
+
+#nav a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  display: block;
+  color: rgb(51, 74, 90);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+#nav li a:hover {
+  /*background-color: rgb(72, 73, 60);*/
+  background-color: rgb(73, 104, 122);
+}
+</style>
